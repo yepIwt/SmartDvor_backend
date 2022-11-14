3e93115615cf0d18f3fa81054ba4857d3fc17c12
Sergievsky Nikita <ReetRay@protonmail.com>
2022-11-14 03:30:50 +0300

    Feat: API вывод профиля пользователя

--- api/urls.py
+++ api/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register', views.RegisterView.as_view()),
     path('login', views.LoginView.as_view()),
     path('logout', views.LogoutView.as_view()),
+    path('user', views.UserView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -66,3 +66,20 @@ class LogoutView(APIView):
             "message": "success"
         }
         return response
+
+class UserView(APIView):
+    def get(self, request):
+
+        token = request.COOKIES.get("jwt")
+
+        if not token:
+            raise AuthenticationFailed("Unauthenticated!")
+
+        try:
+            payload = jwt.decode(token, "secret", algorithms = ["HS256"])
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed("Unauthenticated!")
+
+        user = User.objects.filter(id = payload["id"]).first()
+        serializer = serializers.UserSerializer(user)
+        return Response(serializer.data)
